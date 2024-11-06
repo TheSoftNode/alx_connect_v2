45dafd0de7f67c15754171f5b698fc94af90b234
TheSoftNode <thesoftnode@gmail.com>
2024-11-06 11:58:03 +0100

    Configured gmail as smtp server

--- alx_connect_be/settings.py
+++ alx_connect_be/settings.py
@@ -133,10 +133,10 @@ AUTHENTICATION_BACKENDS = ['users.auth_backends.EmailBackend']
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'your-smtp-server'
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@example.com'
-EMAIL_HOST_PASSWORD = 'your-password'
-DEFAULT_FROM_EMAIL = 'your-email@example.com'
+EMAIL_HOST_USER = 'theo.uche2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'ovwjxoaoxswvdoyc'
+DEFAULT_FROM_EMAIL = 'theo.uche2023@gmail.com'
 
